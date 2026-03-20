981e526fc4ab9186def0c94fcf65e44b4457e254
Gamba <juanca.quinterom@gmail.com>
2026-03-20 00:07:57 -0500

    Singleton Behaviour

---
+++ Assets/Gamba Utilities/Core/Behaviours/SingletonBehaviour.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace GambaUtilities
+{
+	/// <summary> Ensures that <typeparamref name="T"/> can only have one active instance. </summary>
+	public abstract class SingletonBehaviour<T> : MonoBehaviour
+		where T : SingletonBehaviour<T>
+	{
+		private static T instance;
+
+		public static T Instance => instance ?? FindInstance() ?? BuildInstance();
+
+		#region Instance
+
+		private static T FindInstance()
+		{
+			return instance = FindObjectOfType<T>();
+		}
+
+		private static T BuildInstance()
+		{
+			GameObject gameObject = new GameObject(typeof(T).Name);
+
+			return instance = gameObject.AddComponent<T>();
+		}
+
+		#endregion
+
+		// ----------------------------------------------------------------------------------------------------
+
+		#region Init
+
+		private void Awake()
+		{
+			if (instance == null) instance = this as T;
+			else if (this is T && instance != this) Destroy(gameObject);
+		}
+
+		#endregion
+
+	}
+}
